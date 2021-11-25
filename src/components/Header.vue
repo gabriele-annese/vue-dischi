4d5a0bdec84747a9677179fa2d96f3dde36b1ced
gabriele-annese <gannese06@gmail.com>
2021-11-25 14:19:42 +0100

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="p-2">
+      <div class="logo">
+         <img src="@/assets/logo (1).png" alt="">
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+name : "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    background-color: #2e3a46;
+    .logo{
+        width: 100px;
+        height: 100px;
+        img{
+            width: 100%;
+            height: 100%;
+            object-fit: contain;
+        }
+    }
+}
+</style>
